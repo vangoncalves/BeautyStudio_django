ddc0cf282892f149cc3e08b8ed9f07af0bcaca6a
Vanessa Gonçalves <vangoncalvesilva@gmail.com>
2024-09-01 14:55:26 -0300

    Delete BStudio directory

--- BStudio/BStudios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('cursos', views.cursos, name='cursos'),
-    path('servicos', views.servicos, name='servicos')
-]
