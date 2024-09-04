34f5eecc5bac432d4c682941ca0f9798336d2c45
vangoncalves <vangoncalvesilva@gmail.com>
2024-09-03 21:55:34 -0300

    refiz pela 3 vez aqui no git, espero que seja a ultima,aaaaaaa

---
+++ BStudio/BStudios/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from BStudios.models import *
+
+admin.site.register(Usuario)
+admin.site.register(CaCursos)
+admin.site.register(Funcionario)
+admin.site.register(Curso)
+admin.site.register(MetodoPagamento)
+admin.site.register(Agendamento)
+admin.site.register(NotaFiscal)
+admin.site.register(Comprovante)
---
+++ BStudio/BStudios/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cursos', views.cursos, name='cursos'),
+    path('cursos/<idCaCursos>/', views.curso, name='curso'),
+    path('curso/<idCurso>/', views.detalhes, name='detalhes'),
+    path('agendamentos', views.agendamentos, name='agendamentos'),
+    path('cursoadd', views.cursoadd, name='cursoadd'),
+    path('cursosadd', views.cursosadd, name='cursosadd'),
+]
